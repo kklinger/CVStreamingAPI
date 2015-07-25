5bd7a60e5663fd60653a27381d011f4b306f2949
kklinger <kklinger@causeview.com>
2015-07-24 22:09:26 -0400

    Initial commit of simple functional version

---
+++ CVStreamingAPI/Controllers/CVStreamChartController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CVStreamingAPI.Models;
+
+namespace CVStreamingAPI.Controllers
+{
+    public class CVStreamChartController : Controller
+    {
+        //
+        // GET: /Test1/
+
+        public ActionResult Index(string id)
+        {
+            var myModel = new CVStreamChart();
+            myModel.name = "KlingerDev " + id;
+            myModel.now = DateTime.Now;
+            return View(myModel);
+        }
+
+    }
+}
---
+++ CVStreamingAPI/Models/CVStreamChartModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CVStreamingAPI.Models
+{
+    public class CVStreamChart
+
+    {
+        public string name;
+        public DateTime now;
+    }
+}
